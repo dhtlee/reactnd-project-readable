1436dacdd061870a93cdd02c966d5882140adef6
dhtlee <dhtlee@rocketmail.com>
2017-09-12 08:30:37 -0400

    DL: Add redux store and redux dev tool support.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'
+
 import 'index.css';
 import App from 'components/app';
 import registerServiceWorker from 'registerServiceWorker';
+import reducers from 'reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducers,
+  composeEnhancers(
+    applyMiddleware()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
