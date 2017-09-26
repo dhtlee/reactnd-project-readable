a80002d213da93221109a5e9f52dd482dcd5a19b
dhtlee <dhtlee@rocketmail.com>
2017-09-26 09:33:53 -0400

    DL: Display generic message if there are no posts to display.

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -3,11 +3,14 @@ import React from 'react';
 import Post from './post';
 
 const PostList = ({ posts }) => (
-  <div className='post-list'>
-    {posts.map(post => 
-      <Post key={post.id} {...post} />
-    )}
-  </div>
+  posts.length === 0 ? 
+    <p>Whoops! There are no posts to display!</p>
+    :
+    <div className='post-list'>
+      {posts.map(post => 
+        <Post key={post.id} {...post} />
+      )}
+    </div>
 )
 
 export default PostList;
