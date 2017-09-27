785485abe7efeb29bb4d85e703eebbcabe78c1f1
dhtlee <dhtlee@rocketmail.com>
2017-09-26 20:36:01 -0400

    DL: Implement sorting for posts.

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import Sorter from './sorter';
 import Post from './post';
 
-const PostList = ({ posts }) => (
-  <div className='post-list'>
-    <Sorter />
-    {posts.length === 0 ? 
-      <p>Whoops! There are no posts to display!</p>
-      :
-      posts.map(post => 
-        <Post key={post.id} showDetail={false} {...post} />
-      )
-    }
-  </div>
-)
+const PostList = ({ posts, location: { search } }) => {
+  const params = new URLSearchParams(search);
+  const sortBy = params.get('sort');
+  if (!!search && posts.length !== 0) {
+    posts.sort((post1, post2) => post2[sortBy] - post1[sortBy]);
+  }
+  
+  return (
+    <div className='post-list'>
+      <Sorter />
+      {posts.length === 0 ? 
+        <p>Whoops! There are no posts to display!</p>
+        :
+        posts.map(post => 
+          <Post key={post.id} showDetail={false} {...post} />
+        )
+      }
+    </div>
+  )
+}
 
-export default PostList;
+export default withRouter(PostList);
