640cda59c773bb2ed4a9facfa27dc33a236dc043
dhtlee <dhtlee@rocketmail.com>
2017-10-02 22:51:32 -0400

    DL: Add UI for PostForm.

--- src/components/post-form.js
+++ src/components/post-form.js
@@ -1,27 +1,72 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
 
+import { capitalizeFirst } from 'utils/helper';
+
 const PostForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props;
+  const { handleSubmit, pristine, submitting, categories, history } = props;
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" component="input" type="text" />
+    <form className='form' onSubmit={handleSubmit}>
+      <div className='form-field'>
+        <label>Title</label>
+        <div className='form-field-input'>
+          <Field
+            name="title"
+            component="input"
+            type="text"
+            placeholder="Super duper awesome title"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" component="input" type="text" />
+      <div className='form-field'>
+        <label>Author</label>
+        <div className='form-field-input'>
+          <Field
+            name="author"
+            component="input"
+            type="text"
+            placeholder="Name that is going to be famous!"
+          />
+        </div>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
+      <div className='form-field'>
+        <label>Category</label>
+        <div className='form-field-input'>
+          <Field name="category" component="select">
+            {categories.map((category, index) => 
+              <option key={index} value={category.name}>{capitalizeFirst(category.name)}</option>
+            )}
+          </Field>
+        </div>
       </div>
-      <button type="submit">Submit</button>
+      <div className='form-field'>
+        <label>Body</label>
+        <div className='form-field-input'>
+          <Field
+            name="body"
+            component="textarea"
+            placeholder="Write your life story here"
+          />
+        </div>
+      </div>
+      <div className='form-field'>
+        <button type="button" onClick={() => history.goBack()}>
+          Back
+        </button>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+      </div>
     </form>
   )
 }
 
+const mapStateToProps = ({ categories }) => ({
+  categories
+})
+
 export default reduxForm({ 
   form: 'post'
-})(PostForm);
+})(withRouter(connect(mapStateToProps)(PostForm)));
