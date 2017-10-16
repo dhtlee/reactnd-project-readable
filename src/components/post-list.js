dc73e2bde53819b17b5a7ce3013d77aa8805ed38
dhtlee <dhtlee@rocketmail.com>
2017-10-15 20:18:19 -0400

    DL: HOC example in post-list and quick-links.

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -5,24 +5,27 @@ import { CONTENT_POSTS } from 'actions/constants';
 import Sorter from './sorter';
 import PostSummary from './post-summary';
 
-const withPostListEmpty = (WrappedComponent) => (props) => (
-  props.posts.length === 0 ?
-  <p><em>Whoops! There are no posts to display!</em></p>
+const conditionalRender = (WrappedComponent) => ({ posts, ...props }) => (
+  posts.length === 0 ?
+  <WrappedComponent>
+    <p><em>Whoops! There are no posts to display!</em></p>
+  </WrappedComponent>
   :
-  <WrappedComponent {...props} />
+  <WrappedComponent {...props}>
+    {posts.map(
+      post => <PostSummary key={post.id} {...post} />
+    )}
+  </WrappedComponent>
 )
 
-const PostList = ({ posts }) => (
+const PostList = ({ children }) => (
   <div className='post-list'>
     <div className='post-submenu'>
       <Link to='/posts/new'>New Post</Link>
       <Sorter content={CONTENT_POSTS} />
     </div>
-    {posts.map(post => 
-        <PostSummary key={post.id} {...post} />
-      )
-    }
+    {children}
   </div>
 )
 
-export default withPostListEmpty(PostList);
+export default conditionalRender(PostList);
--- src/components/quick-links.js
+++ src/components/quick-links.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const QuickLinks = ({ location }) => (
-  <div className='sidebar-section'>
-    <div className='sidebar-section-header heading'>
-      <h4>Quick Links</h4>
-    </div>
-    <div className='sidebar-section-content'>
-      <div className='sidebar-section-content-item'>
-      {location.pathname === '/' ?
-        <span>Home</span>
-        :
-        <Link to='/'>Home</Link>
-      }
+const withToggle = (WrappedComponent) => ({ location }) => 
+  location.pathname === '/' ?
+  <WrappedComponent>
+    <span>Home</span>
+  </WrappedComponent>
+  :
+  <WrappedComponent>
+    <Link to='/'>Home</Link>
+  </WrappedComponent>
+
+const QuickLinks = ({ children }) => {
+  return (
+    <div className='sidebar-section'>
+      <div className='sidebar-section-header heading'>
+        <h4>Quick Links</h4>
+      </div>
+      <div className='sidebar-section-content'>
+        <div className='sidebar-section-content-item'>
+          {children}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
-export default withRouter(QuickLinks);
+export default withRouter(withToggle(QuickLinks));
